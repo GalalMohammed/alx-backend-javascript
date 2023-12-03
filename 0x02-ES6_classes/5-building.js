a6170886d56c1572951a1c7bbb53f0ed5ef2c50b
GalalMohammed <galalmoh2001@gmail.com>
2023-12-04 00:27:40 +0200

    add task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,17 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
+
+  evacuationWarningMessage() {
+    throw Error("Class extending Building must override evacuationWarningMessage");
+  }
+}
