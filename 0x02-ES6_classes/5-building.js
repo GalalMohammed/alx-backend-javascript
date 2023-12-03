80e8f55533f4c26670aa13bce9e091ddbcd49480
GalalMohammed <galalmoh2001@gmail.com>
2023-12-04 00:48:56 +0200

    except base

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 export default class Building {
   constructor(sqft) {
-    if (this.evacuationWarningMessage == undefined) {
-      throw Error("Class extending Building must override evacuationWarningMessage");
+    if (this.constructor.name !== 'Building' && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
   }
