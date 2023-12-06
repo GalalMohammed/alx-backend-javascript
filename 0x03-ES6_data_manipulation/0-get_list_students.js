bc18a9caf675149e07b1d7e22db428af17c45d1a
GalalMohammed <galalmoh2001@gmail.com>
2023-12-06 06:56:45 +0200

    fix lint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,15 @@
 export default function getListStudents() {
-  return [ {
+  return [{
     firstName: 'Guillaume',
     id: 1,
-    location: 'San Francisco'
+    location: 'San Francisco',
   }, {
     firstName: 'James',
     id: 2,
-    location: 'Columbia'
+    location: 'Columbia',
   }, {
     firstName: 'Serena',
     id: 5,
-    location: 'San Francisco'
-  } ];
+    location: 'San Francisco',
+  }];
 }
