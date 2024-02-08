58cc538201c256af50d85b5628d541b5ecf867ac
GalalMohammed <galalmoh2001@gmail.com>
2024-02-09 00:27:32 +0200

    fix lint

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,29 +3,30 @@ import readDatabase from '../utils';
 export default class StudentsController {
   static getAllStudents(_request, response) {
     readDatabase(process.argv[2]).then((fieldCounts) => {
+      let res = '';
       Object.entries(fieldCounts).forEach(([field, { count, list }]) => {
         res += `\nNumber of students in ${field}: ${count}. List: ${list.join(', ')}`;
-        response.send(`This is the list of our students\n${res}`);
       });
+      response.send(`This is the list of our students${res}`);
     }).catch((error) => {
-        // Handle errors related to reading the database file
-        response.status(500);
-        response.send(error.message);
-      });
+      // Handle errors related to reading the database file
+      response.status(500);
+      response.send(error.message);
+    });
   }
-  
+
   static getAllStudentsByMajor(request, response) {
-    const major = request.params.major;
+    const { major } = request.params;
     if (major !== 'CS' && major !== 'SWE') {
       response.status(500);
       response.send('Major parameter must be CS or SWE');
     }
     readDatabase(process.argv[2]).then((fieldCounts) => {
-        response.send(`List: ${fieldCounts[major].list.join(', ')}`);
+      response.send(`List: ${fieldCounts[major].list.join(', ')}`);
     }).catch((error) => {
-        // Handle errors related to reading the database file
-        response.status(500);
-        response.send(error.message);
-      });
+      // Handle errors related to reading the database file
+      response.status(500);
+      response.send(error.message);
+    });
   }
 }
