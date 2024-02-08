40b0cfb04180ea0bbc81b99fda84ee970b48516f
GalalMohammed <galalmoh2001@gmail.com>
2024-02-08 20:35:06 +0200

    use const

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', () => {
-  let chunk = process.stdin.read();
+  const chunk = process.stdin.read();
   if (chunk) {
     process.stdout.write(`Your name is: ${chunk}`);
   }
