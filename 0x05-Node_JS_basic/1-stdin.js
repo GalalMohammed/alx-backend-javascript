6688a835b39e479c4fcfd70500ef3f7879489dc5
GalalMohammed <galalmoh2001@gmail.com>
2024-02-08 20:30:48 +0200

    use process to catch EOF

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,14 +1,12 @@
-const readline = require('readline');
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', () => {
+  let chunk = process.stdin.read();
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
-console.log("Welcome to Holberton School, what is your name?");
-r1.on('line', (input) => {
-  console.log(`Your name is: ${input}`);
-  r1.close();
-});
-r1.on('close', () => {
-  console.log("This important software is now closing");
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
   process.exit();
 });
