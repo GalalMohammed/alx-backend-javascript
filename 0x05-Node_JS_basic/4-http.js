2954b3a31e8a8cc92b1b646fd2d5d32e14b16e70
GalalMohammed <galalmoh2001@gmail.com>
2024-02-08 22:01:43 +0200

    add task 4

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+const http = require('node:http');
+
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the response headers
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // Send the reponse body
+  res.end('Hello Holberton School!');
+});
+
+// Listen on port 1245
+server.listen(1245);
+
+// Export the server variable
+module.exports = server;
