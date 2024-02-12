db6e0079208cb6f808d8873d3f9f78f299f6ce3f
GalalMohammed <galalmoh2001@gmail.com>
2024-02-12 22:19:54 +0200

    add task 9

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,32 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', function () {
+  it('should return correct status code and message for GET /', function (done) {
+    request.get('http://localhost:7865', (_err, response, body) => {
+      // Assertion
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
+
+describe('Cart Page', function () {
+  it('should return correct status code and message when :id is a number', function (done) {
+    request.get('http://localhost:7865/cart/123', (_err, response, body) => {
+      // Assertion
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart :123');
+      done();
+    });
+  });
+
+  it('should return 404 status code when :id is NOT a number', function (done) {
+    request.get('http://localhost:7865/cart/hello', (_err, response) => {
+      // Assertion
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
