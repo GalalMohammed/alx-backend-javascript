52f903b0eea749020fc446a84a5f3f1ced119f8b
GalalMohammed <galalmoh2001@gmail.com>
2024-02-12 22:01:21 +0200

    add task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,14 @@
+const request = require('request');
+const { expect } = require('chai');
+const app = require('./api');
+
+describe('Index page', function () {
+  it('should return correct status code and message for GET /', function (done) {
+    request.get('http://localhost:7865', (_err, response, body) => {
+      // Assertion
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
