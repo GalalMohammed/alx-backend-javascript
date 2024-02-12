b6d9c5db1a05d7bf33c00182f130292f6ac05615
GalalMohammed <galalmoh2001@gmail.com>
2024-02-12 19:17:11 +0200

    add a test suite

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,6 +4,7 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function () {
   it('checks equality', function () {
     assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(1, 3.2), 4);
     assert.equal(calculateNumber(1, 3.7), 5);
     assert.equal(calculateNumber(1.2, 3.7), 5);
     assert.equal(calculateNumber(1.5, 3.7), 6);
